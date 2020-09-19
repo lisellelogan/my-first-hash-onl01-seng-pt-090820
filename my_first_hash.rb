63a529a709c04b2ef7dfde4010ea79cbf493e1c5
Liselle Logan <liselle.logan@gmail.com>
2020-09-19 02:44:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,9 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  dog_names = {
+    "corgi" => "kobe",
+    "jindo" => "gomi",
+    "golden retriever" => "appa"
+  }
 
   
 end
